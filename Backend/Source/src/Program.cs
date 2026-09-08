c2f18dc49e3c065aa18ce9db76dbb5bb4a7e5b6f
Phan Gia Bảo <phangia223@gmail.com>
2026-09-08 13:05:04 +0700

    siuuuu

--- Backend/Source/src/Program.cs
+++ Backend/Source/src/Program.cs
@@ -73,7 +73,8 @@ var connectionString = "Server=todoapp-mysql-phangia223-d258.l.aivencloud.com;Po
 MySqlConnectionStringBuilder csb = new MySqlConnectionStringBuilder(connectionString)
 {
     SslMode = MySqlSslMode.Required,
-    AllowPublicKeyRetrieval = true
+    AllowPublicKeyRetrieval = true,
+    SslCa = string.Empty
 };
 connectionString = csb.ConnectionString;
 
